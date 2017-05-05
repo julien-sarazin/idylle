eb91fc64efa4400ddd2f6702d7cf0b09ba14cb32
Julien Sarazin <julien.sarazin@me.com>
2017-05-05 19:39:09 +0200

    udpated tests

---
+++ test/ContextBuilder.specs.js
@@ -0,0 +1,33 @@
+const SUT = require('../lib/ContextBuilder');
+const expect = require('expect');
+const describe = require('mocha').describe;
+const it = require('mocha').it;
+
+describe('Context', () => {
+    it('should NOT tolerate empty request or response', () => {
+        const req = null;
+        const res = null;
+
+        expect(() => new SUT().build(req, res)).toThrow()
+    });
+
+    it('should forward request properties to the context', () => {
+        const req = {
+            body: {some: 'data'},
+            files: {some: 'files'},
+            token: {some: 'token'}
+        };
+
+        const res = {};
+        const sut = new SUT().build(req, res);
+
+        expect(sut.data).toEqual(req.body);
+        expect(sut.files).toEqual(req.files);
+        expect(sut.token).toEqual(req.token);
+    });
+
+    it('should use the registered CriteriaBuilder to generate criteria', (done) => {
+        const FakeCriteriaBuilder = { build : () => { done() }};
+        new SUT(FakeCriteriaBuilder).build({}, {})
+    });
+});
