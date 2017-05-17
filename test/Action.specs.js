261f1da2447859474d4761dc9bba0f04156440a1
Julien Sarazin <julien.sarazin@me.com>
2017-05-17 22:14:16 +0200

    updated tests

--- test/Action.specs.js
+++ test/Action.specs.js
@@ -1,8 +1,22 @@
-const SUT       = require('../index').Action;
-const expect    = require('expect');
-const describe  = require('mocha').describe;
-const it        = require('mocha').it;
+const SUT = require('../index').Action;
+const expect = require('expect');
+const describe = require('mocha').describe;
+const it = require('mocha').it;
 
 describe('Action', () => {
-   // Todo..
+    describe('when the execute property is not set', () => {
+        it('should throw an exception', () => {
+            expect(() => {
+                SUT()
+            }).toThrow()
+        });
+    });
+
+    describe('when the execute property is not a function', () => {
+        it('should throw an exception', () => {
+            expect(() => {
+                SUT({execute: 'foo'})
+            }).toThrow()
+        })
+    });
 });
