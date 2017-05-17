4b635675f64308098d29a847ad83c8b0ec23a159
Julien Sarazin <julien.sarazin@me.com>
2017-05-17 21:35:21 +0200

    refactoring, increasing code quality

---
+++ test/Action.specs.js
@@ -0,0 +1,8 @@
+const SUT       = require('../index').Action;
+const expect    = require('expect');
+const describe  = require('mocha').describe;
+const it        = require('mocha').it;
+
+describe('Action', () => {
+   // Todo..
+});
