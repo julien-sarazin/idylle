221e186e2111620c87c36d6ac156aa21eb87bbc0
Julien Sarazin <julien.sarazin@me.com>
2017-05-18 11:35:18 +0200

    updated Action tests

--- test/Action.specs.js
+++ test/Action.specs.js
@@ -2,6 +2,8 @@ const SUT = require('../index').Action;
 const expect = require('expect');
 const describe = require('mocha').describe;
 const it = require('mocha').it;
+const request = require('supertest');
+const express = require('express');
 
 describe('Action', () => {
     describe('when the execute property is not set', () => {
@@ -22,14 +24,20 @@ describe('Action', () => {
 
     describe('when an action is built', () => {
         it('should return a function', () => {
-            const sut = SUT({execute: () => {}});
+            const sut = SUT({
+                execute: () => {
+                }
+            });
             expect(() => {
                 sut();
             }).toNotThrow()
         });
 
         it('should have an expose method', () => {
-            const sut = SUT({execute: () => {}});
+            const sut = SUT({
+                execute: () => {
+                }
+            });
             expect(sut.expose).toNotBe(undefined);
         });
     });
@@ -37,8 +45,59 @@ describe('Action', () => {
     describe('when cache options is given', () => {
         it('should set the cache property to the returned function', () => {
             let cacheOpts = {foo: 'bar'};
-            const sut = SUT({execute: () => {}, cache: cacheOpts});
+            const sut = SUT({
+                execute: () => {
+                }, cache: cacheOpts
+            });
             expect(sut.cache).toEqual(cacheOpts);
         })
+    });
+
+    describe('when an action is executed', () => {
+        it('should have normalized context', () => {
+            let cacheOpts = {foo: 'bar'};
+            const sut = SUT({
+                execute: (context) => {
+                    expect(context.criteria).toNotBe(undefined);
+                }, cache: cacheOpts
+            });
+
+            sut({});
+        })
+    });
+
+    describe('when a valid action is exposed', () => {
+        it('should be callable from a middleware', (done) => {
+            const sut = SUT({
+                execute: (context) => {
+                    return Promise.resolve(true);
+                }
+            });
+
+            const collab = express();
+            collab.use((req, res, next) => {res.submit = res.send; next();}, sut.expose());
+
+            request(collab)
+                .get('/')
+                .expect(200, done)
+        });
+    });
+
+
+    describe('when an invalid action is exposed', () => {
+        it('should be callable from a middleware', (done) => {
+            const sut = SUT({
+                execute: (context) => {
+                    return Promise.reject('suce');
+                }
+            });
+
+            const collab = express();
+            collab.use((req, res, next) => {res.error = (err) => {res.status(500).send()}; next();}, sut.expose());
+
+            request(collab)
+                .get('/')
+                .expect(500, done)
+        });
     });
 });
