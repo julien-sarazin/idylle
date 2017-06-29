ead4224e3d46e377e2e2e92ffad91f7aef5cf9f6
Julien Sarazin <julien.sarazin@me.com>
2017-06-29 18:18:05 +0200

    updated ErrorHanlder, added Custom ActionError

--- test/ErrorHandler.specs.js
+++ test/ErrorHandler.specs.js
@@ -5,6 +5,7 @@ const request = require('supertest');
 const express = require('express');
 
 const ErrorHandler = require('../lib/ErrorHandler');
+const ActionError = require('../lib/ActionError');
 
 describe('ErrorHandler', () => {
     describe('when no error is passed', () => {
@@ -33,7 +34,8 @@ describe('ErrorHandler', () => {
                 // Setup
                 const SUT = ErrorHandler.bind(ErrorHandler, req, res);
                 // Use
-                return Promise.reject({code: 503})
+                let error = new ActionError(null, 503);
+                return Promise.reject(error)
                     .catch(SUT);
             });
 
@@ -42,9 +44,9 @@ describe('ErrorHandler', () => {
                 .expect(503, done)
         });
 
-        it('should use the error stack as reason if available', (done) => {
+        it('should call use the toJSON() as response body is the error inherit from ActionError', (done) => {
             const collab = express();
-            const msg = 'Error: this is an official error message';
+            const msg = 'this is an official error message';
 
             collab.use((req, res, next) => {
                 // Setup
@@ -52,7 +54,7 @@ describe('ErrorHandler', () => {
                 // Use
                 return Promise.resolve()
                     .then(() => {
-                        throw new Error(msg);
+                        throw new ActionError(msg);
                     })
                     .catch(SUT);
             });
@@ -60,28 +62,32 @@ describe('ErrorHandler', () => {
             request(collab)
                 .get('/')
                 .expect(500)
-                .then(response => {
-                    expect(response.error.text)
-                        .toInclude(msg)
+                .expect(res => {
+                    expect(res.body.reason).toEqual(msg);
+                    expect(res.body.stack).toNotBe(undefined);
                 })
-                .then(done)
+                .end(done)
         });
 
-        it('should use the reason property as HTTP Body response', (done) => {
+        it('should dump the error in an error object', (done) => {
             const collab = express();
-            const msg = 'this could be a reason';
+            const error = {msg: 'custom error', code: 11000};
 
             collab.use((req, res, next) => {
                 // Setup
                 const SUT = ErrorHandler.bind(ErrorHandler, req, res);
                 // Use
-                return Promise.reject({code: 503, reason: msg})
+                return Promise.reject(error)
                     .catch(SUT);
             });
 
             request(collab)
                 .get('/')
-                .expect(503, msg, done)
+                .expect(res => {
+                    expect(res.statusCode).toBe(500);
+                    expect(res.body.error).toEqual(error);
+                })
+                .end(done)
         });
     });
 });
