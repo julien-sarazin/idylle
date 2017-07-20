bfed49db7789425d3beeb76eaf82da1451dc556a
Julien Sarazin <julien.sarazin@me.com>
2017-07-20 14:52:05 +0200

    updated dependencies, tests, and utils

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports.Core = require('./lib/Core');
 module.exports.Action = require('./lib/Action');
 module.exports.Context = require('./lib/Context');
+module.exports.Utils = require('./lib/utils');
