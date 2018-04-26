deb74bd2ca9e3cb270cc860a04e4e62e534849b3
Julien Brouzes <julien.brouzes1@gmail.com>
2018-04-26 16:21:13 +0200

    feature/io > Starting refacto

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports.Core = require('./lib/Core');
 module.exports.Action = require('./lib/Action');
-module.exports.Context = require('./lib/Context');
+module.exports.Context = require('./lib/HTTPContext');
 module.exports.Utils = require('./lib/utils');
